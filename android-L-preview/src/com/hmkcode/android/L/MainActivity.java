f2b8cdca35174342b2a2316435739b42551084e5
hmkcode <hmkcode@gmail.com>
2014-07-06 23:11:58 +0300

    l preview

---
+++ android-L-preview/src/com/hmkcode/android/L/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hmkcode.android.L;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
