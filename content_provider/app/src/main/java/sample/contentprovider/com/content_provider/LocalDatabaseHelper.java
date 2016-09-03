23ac91fe5991766507fb2a1f02a4899ebd3b98ea
rajesh65 <rajeshxreach@gmail.com>
2016-09-03 15:27:10 +0530

    added database helper

---
+++ content_provider/app/src/main/java/sample/contentprovider/com/content_provider/LocalDatabaseHelper.java
@@ -0,0 +1,33 @@
+package sample.contentprovider.com.content_provider;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Ankit on 9/3/2016.
+ */
+public class LocalDatabaseHelper extends SQLiteOpenHelper {
+
+    public LocalDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    private static final String SQL_CREATE_MAIN = "CREATE TABLE "+
+            "main "+    // Table`s name
+            " ( "+      //The columns in the table
+            " _ID INTEGER PRIMARY KEY, "+
+            " WORD TEXT, "+
+            "FREQUENCY INTEGER, "+
+            " LOCALE TEXT)";
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_MAIN);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
--- content_provider/app/src/main/java/sample/contentprovider/com/content_provider/YourProvider.java
+++ content_provider/app/src/main/java/sample/contentprovider/com/content_provider/YourProvider.java
@@ -2,39 +2,74 @@ package sample.contentprovider.com.content_provider;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
  * Created by Ankit on 9/3/2016.
  */
 public class YourProvider extends ContentProvider {
+
     private static final UriMatcher sUriMatcher =  new UriMatcher(UriMatcher.NO_MATCH);
+    private static final String DBNAME = "songsDB";
+    private SQLiteDatabase db;
+    private LocalDatabaseHelper mOpenHelper;
 
     static {
         sUriMatcher.addURI("com.example.app.provider.me","songs",1);
         sUriMatcher.addURI("com.example.app.provider.me","songs/#",2);
     }
 
-    @Override
-    public boolean onCreate() {
-        return false;
+    private static Context context;
+    private static YourProvider mInstance;
+
+    public YourProvider(Context context) {
+        this.context = context;
+    }
+
+    public static YourProvider getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new YourProvider(context);
+        }
+        return mInstance;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-        return null;
+    public boolean onCreate() {
+
+        mOpenHelper = new LocalDatabaseHelper(
+                context,
+                DBNAME,
+                null,
+                1);
+
+
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        // if uri matches for single row : vnd.android.cursor.item/
+        // if uri matches for more than one row : vnd.android.cursor.dir/
+        // Provider specific part : vnd.<name>.<type>
+        // for single row of table vnd.android.cursor.item/vnd.com.example.ourapp.provider.table
+
     }
 
+    //Please do some real coding, this is not upto the level of your standard.
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+
+        db = mOpenHelper.getWritableDatabase();
+
     }
 
     @Override
