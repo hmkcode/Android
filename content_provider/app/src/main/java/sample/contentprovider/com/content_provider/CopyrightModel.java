2848ca39106deaacf6ea93b8c2cdb24aaf29aa06
rajesh65 <rajeshxreach@gmail.com>
2016-09-04 17:18:31 +0530

    getter and setter

--- content_provider/app/src/main/java/sample/contentprovider/com/content_provider/CopyrightModel.java
+++ content_provider/app/src/main/java/sample/contentprovider/com/content_provider/CopyrightModel.java
@@ -20,4 +20,51 @@ class CopyrightModel {
 
 	}
 
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public String getLicence() {
+		return licence;
+	}
+
+	public void setLicence(String licence) {
+		this.licence = licence;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getLicence_url() {
+		return licence_url;
+	}
+
+	public void setLicence_url(String licence_url) {
+		this.licence_url = licence_url;
+	}
+
+	public String getHolder_url() {
+		return holder_url;
+	}
+
+	public void setHolder_url(String holder_url) {
+		this.holder_url = holder_url;
+	}
 }
--- content_provider/app/src/main/java/sample/contentprovider/com/content_provider/CreatorModel.java
+++ content_provider/app/src/main/java/sample/contentprovider/com/content_provider/CreatorModel.java
@@ -10,4 +10,11 @@ class CreatorModel {
 
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
