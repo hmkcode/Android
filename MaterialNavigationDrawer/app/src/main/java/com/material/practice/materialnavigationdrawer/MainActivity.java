55dac38a247266e53d06a292cd8b22825e10f4eb
rajesh65 <rajeshxreach@gmail.com>
2016-09-09 02:01:37 +0530

    changed items of nav

--- MaterialNavigationDrawer/app/src/main/java/com/material/practice/materialnavigationdrawer/MainActivity.java
+++ MaterialNavigationDrawer/app/src/main/java/com/material/practice/materialnavigationdrawer/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initializeComponents();
-        setUpNavigationDrawer();
+       // setUpNavigationDrawer();
 
     }
 
