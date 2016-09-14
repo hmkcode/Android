6cbb895559575e75ab2a35a8346552fc0c6d8307
rijved <rijved@gmail.com>
2016-09-14 15:19:04 +0530

    added exception calls

--- MyApplication/app/src/main/java/simple/musicgenie/CentralDataRepository.java
+++ MyApplication/app/src/main/java/simple/musicgenie/CentralDataRepository.java
@@ -3,6 +3,7 @@ package simple.musicgenie;
 
 import android.content.Context;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 
 public class CentralDataRepository {
 
@@ -34,11 +35,13 @@ public class CentralDataRepository {
     private static final int TYPE_RESULT = 406;
 
     private ActionCompletedListener mListener;
+    private DataReadyToSubmitListener dataReadyToSubmitListener;
 
     private static Context context;
 
     private static CentralDataRepository mInstance;
 
+    private int mLastLoadedType = TYPE_TRENDING;
 
     /**
      * Default constructor
@@ -68,16 +71,104 @@ public class CentralDataRepository {
      * @param type Flag
      * @param callback  Callback for action completed
      */
-    public void submitAction(int type , ActionCompletedListener callback){
 
+    public void submitAction(int type , ActionCompletedListener callback) throws InvalidCallbackException{
 
+        if(callback!=null)
+            setListener(callback);
+        else throw new InvalidCallbackException("Callback in Invalid");
+
+        switch (type){
+
+            case FLAG_FIRST_LOAD:
+                                    loadTrendingOrRequestTrending();
+                break;
+            case FLAG_RESTORE:
+                                    submitLastLoaded();
+                break;
+            case FLAG_SEARCH:
+                                    searchAndSubmit();
+                break;
+            case FLAG_REFRESS:
+                                    refressAndSubmit();
+                break;
+            default:
+                break;              // do nothing
+        }
 
     }
 
+    /**
+     * gets Last Loaded and Request
+     *  save to db + callback + submit
+     *  not need to set Last loaded
+     */
+    private void refressAndSubmit() {
 
+    }
 
+    /**
+     *  Searches for result and submit + save to db
+     *      + callback
+     *      After submittion must setLastLoaded
+     */
+    private void searchAndSubmit() {
+
+    }
+
+    /**
+     * Checks Last Loaded
+     *      gets from DB and submits to registered adapters
+     *      After submittion must setLastLoaded
+     */
+    private void submitLastLoaded() {
+
+    }
+
+    /**
+     *  Checks DB for saved data
+     *      if !Available
+     *          Request - > save to db + action callback +  adapter callback
+     *      else
+     *          action callback + adapter callback
+     *          After submittion must setLastLoaded
+     */
+    private void loadTrendingOrRequestTrending() {
+
+    }
+
+    public void registerForDataLoadListener(DataReadyToSubmitListener listener){
+        this.dataReadyToSubmitListener = listener ;
+    }
+
+    public void setListener(ActionCompletedListener mListener) {
+        this.mListener = mListener;
+    }
+
+    public int getLastLoadedType() {
+        return this.mLastLoadedType;
+    }
+
+    public void setLastLoadedType(int mLastLoadedType) {
+        this.mLastLoadedType = mLastLoadedType;
+    }
 
     public interface ActionCompletedListener{
         void onActionCompleted();
+    }
+
+    public interface DataReadyToSubmitListener{
+        //TODO: add params to onDataSubmit(ArrayList<SongModel> dataList)
+        void onDataSubmit();
+    }
+
+
+    private class InvalidCallbackException extends Exception {
+        public InvalidCallbackException(String detailMessage) {
+            super(detailMessage);
+            System.out.println(detailMessage);
+            Log.e("CentralDataRepository",detailMessage);
+        }
+
     }
 }
