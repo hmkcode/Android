8d230f554410ea0718c05fe82ebe1f735dc8c56e
rijved <rijved@gmail.com>
2016-09-14 15:35:41 +0530

    added db methods

--- MyApplication/app/src/main/java/simple/musicgenie/DbHelper.java
+++ MyApplication/app/src/main/java/simple/musicgenie/DbHelper.java
@@ -1,18 +1,27 @@
 package simple.musicgenie;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 
 /**
  * Create Local Cache Trending (temp)
  *
  */
-public class DbHelper {
+public class DbHelper extends SQLiteOpenHelper {
 
     private static Context context;
     private static DbHelper mInstance;
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "musicegenieDB";
+    // create table trending ()
 
-    public DbHelper(Context context) {
+    private static final String CREATE_TRENDING_TABLE = "";
+    private static final String CREATE_RESULTS_TABLE = "";
+
+    private DbHelper(Context context) {
+        super(context , DB_NAME , null , DB_VERSION);
         this.context = context;
     }
 
@@ -23,5 +32,19 @@ public class DbHelper {
         return mInstance;
     }
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    /**
+     * @param sqLiteDatabase database object
+     * @param oldVersion    older version of database
+     * @param newVersion    newer version of database
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
 
 }
---
+++ MyApplication/app/src/main/java/simple/musicgenie/SectionModel.java
@@ -0,0 +1,30 @@
+package simple.musicgenie;
+
+
+import java.util.ArrayList;
+
+public class SectionModel {
+
+    String sectionTitle;
+    ArrayList<ItemModel> list;
+
+    public SectionModel(String sectionTitle) {
+        this.sectionTitle = sectionTitle;
+    }
+
+    public String getSectionTitle() {
+        return sectionTitle;
+    }
+
+    public void setSectionTitle(String sectionTitle) {
+        this.sectionTitle = sectionTitle;
+    }
+
+    public ArrayList<ItemModel> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<ItemModel> list) {
+        this.list = list;
+    }
+}
