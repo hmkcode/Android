ecd128abc21f0b8a717b2a4dade028e6dde82254
rijved <rijved@gmail.com>
2016-09-16 01:45:33 +0530

    adding indent

--- MyApplication/app/src/main/java/simple/musicgenie/CloudManager.java
+++ MyApplication/app/src/main/java/simple/musicgenie/CloudManager.java
@@ -1,259 +1,259 @@
-package simple.musicgenie;
+        package simple.musicgenie;
 
 
-import android.content.ClipData;
-import android.content.Context;
+        import android.content.ClipData;
+        import android.content.Context;
 
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+        import com.android.volley.DefaultRetryPolicy;
+        import com.android.volley.Request;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.StringRequest;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
+        import java.net.URL;
+        import java.net.URLEncoder;
+        import java.util.ArrayList;
 
-public class CloudManager {
+        public class CloudManager {
 
-    private static final int SERVER_TIMEOUT_LIMIT = 20000;
-    private static final String TIME_SINCE_UPLOADED_LEFT_VACCANT = "";
-    private static Context context;
-    private static CloudManager mInstance;
-    private DbHelper dbHelper;
-    public CloudManager(Context context) {
-        this.context = context;
-        this.dbHelper = DbHelper.getInstance(context);
-    }
+            private static final int SERVER_TIMEOUT_LIMIT = 20000;
+            private static final String TIME_SINCE_UPLOADED_LEFT_VACCANT = "";
+            private static Context context;
+            private static CloudManager mInstance;
+            private DbHelper dbHelper;
+            public CloudManager(Context context) {
+                this.context = context;
+                this.dbHelper = DbHelper.getInstance(context);
+            }
 
-    public static CloudManager getInstance(Context context) {
-        if (mInstance == null) {
-            mInstance = new CloudManager(context);
-        }
-        return mInstance;
-    }
+            public static CloudManager getInstance(Context context) {
+                if (mInstance == null) {
+                    mInstance = new CloudManager(context);
+                }
+                return mInstance;
+            }
 
-    ///////////////////////////////////////////////////////////////////////////
-    // Trending Section
-    ///////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////
+            // Trending Section
+            ///////////////////////////////////////////////////////////////////////////
 
-    public void lazyRequestTrending() {
+            public void lazyRequestTrending() {
 
-        // clear out back stored data
-        dbHelper.resetTrendingList();
-        requestSupportedPlaylist();
+                // clear out back stored data
+                dbHelper.resetTrendingList();
+                requestSupportedPlaylist();
 
-    }
+            }
 
-    /**
-     * @param type section for trending e.g. pop , rock etc.
-     */
-    private void requestTrendingType(String type) {
-        int count = 8; // max 25
-        final String url = URLS.URL_TRENDING_API + "?type=" + type + "&number=" + count + "&offset=0";
+            /**
+             * @param type section for trending e.g. pop , rock etc.
+             */
+            private void requestTrendingType(String type) {
+                int count = 8; // max 25
+                final String url = URLS.URL_TRENDING_API + "?type=" + type + "&number=" + count + "&offset=0";
 
-        StringRequest trendingReq = new StringRequest(
-                Request.Method.GET,
-                url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String s) {
-                        handleTrending(s);
+                StringRequest trendingReq = new StringRequest(
+                        Request.Method.GET,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String s) {
+                                handleTrending(s);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+
+                            }
+                        });
+
+                trendingReq.setRetryPolicy(new DefaultRetryPolicy(
+                        SERVER_TIMEOUT_LIMIT,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+                VolleyUtils.getInstance().addToRequestQueue(trendingReq, "trendingReq", context);
+
+            }
+
+            /**
+             * @param response json response for trending items
+             */
+            private void handleTrending(String response) {
+
+                SectionModel trendingResult = null;
+                ItemModel item;
+
+                try {
+
+                    L.m("CM(test trending )", "" + response);
+
+                    JSONObject resObj = new JSONObject(response);
+
+                    int count = Integer.parseInt(resObj.getJSONObject("metadata").getString("count"));
+
+                    String sections = resObj.getJSONObject("metadata").getString("type");
+
+                    JSONObject resultsSubObject = resObj.getJSONObject("results");
+
+                    for (int i = 0; i < count; i++) {       // i represent current section Model item
+
+                        JSONArray typeArray = resultsSubObject.getJSONArray(sections);
+
+                        ArrayList<ItemModel> itemModelArrayList = new ArrayList<>();
+
+                        for (int j = 0; j < typeArray.length(); j++) {      // j represent current item model inside i`th section model
+
+                            JSONObject songObj = (JSONObject) typeArray.get(j);
+                            item = new ItemModel(songObj.getString("title"),
+                                    songObj.getString("length"),
+                                    songObj.getString("uploader"),
+                                    songObj.getString("thumb"),
+                                    songObj.getString("get_url"),
+                                    TIME_SINCE_UPLOADED_LEFT_VACCANT,
+                                    songObj.getString("views"),
+                                    sections);
+
+                            itemModelArrayList.add(item);
+
+                        }
+
+                        trendingResult = new SectionModel(sections, itemModelArrayList);
+
+                        //DB write test
+                        L.m("CM(Cloud->DB)", "sending for write");
+                        ArrayList<SectionModel> temp = new ArrayList<>();
+                        temp.add(trendingResult);
+                        dbHelper.addTrendingList(temp);
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
 
-                    }
-                });
 
-        trendingReq.setRetryPolicy(new DefaultRetryPolicy(
-                SERVER_TIMEOUT_LIMIT,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-
-        VolleyUtils.getInstance().addToRequestQueue(trendingReq, "trendingReq", context);
-
-    }
-
-    /**
-     * @param response json response for trending items
-     */
-    private void handleTrending(String response) {
-
-        SectionModel trendingResult = null;
-        ItemModel item;
-
-        try {
-
-            L.m("CM(test trending )", "" + response);
-
-            JSONObject resObj = new JSONObject(response);
-
-            int count = Integer.parseInt(resObj.getJSONObject("metadata").getString("count"));
-
-            String sections = resObj.getJSONObject("metadata").getString("type");
-
-            JSONObject resultsSubObject = resObj.getJSONObject("results");
-
-            for (int i = 0; i < count; i++) {       // i represent current section Model item
-
-                JSONArray typeArray = resultsSubObject.getJSONArray(sections);
-
-                ArrayList<ItemModel> itemModelArrayList = new ArrayList<>();
-
-                for (int j = 0; j < typeArray.length(); j++) {      // j represent current item model inside i`th section model
-
-                    JSONObject songObj = (JSONObject) typeArray.get(j);
-                    item = new ItemModel(songObj.getString("title"),
-                            songObj.getString("length"),
-                            songObj.getString("uploader"),
-                            songObj.getString("thumb"),
-                            songObj.getString("get_url"),
-                            TIME_SINCE_UPLOADED_LEFT_VACCANT,
-                            songObj.getString("views"),
-                            sections);
-
-                    itemModelArrayList.add(item);
-
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
 
-                trendingResult = new SectionModel(sections, itemModelArrayList);
+                L.m("CM(test)", " Trending Type " + trendingResult.sectionTitle);
 
-                //DB write test
-                L.m("CM(Cloud->DB)", "sending for write");
-                ArrayList<SectionModel> temp = new ArrayList<>();
-                temp.add(trendingResult);
-                dbHelper.addTrendingList(temp);
             }
 
+            private void requestSupportedPlaylist() {
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+                final String url = URLS.URL_SUPPORTED_PLAYLIST;
 
-        L.m("CM(test)", " Trending Type " + trendingResult.sectionTitle);
+                StringRequest playlistReq = new StringRequest(
+                        Request.Method.GET,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String s) {
+                                handleSupportedPlaylists(s);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
 
-    }
+                            }
+                        });
 
-    private void requestSupportedPlaylist() {
+                playlistReq.setRetryPolicy(new DefaultRetryPolicy(
+                        SERVER_TIMEOUT_LIMIT,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
-        final String url = URLS.URL_SUPPORTED_PLAYLIST;
+                VolleyUtils.getInstance().addToRequestQueue(playlistReq, "playReq", context);
+            }
 
-        StringRequest playlistReq = new StringRequest(
-                Request.Method.GET,
-                url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String s) {
-                        handleSupportedPlaylists(s);
+            private void handleSupportedPlaylists(String response) {
+
+                //L.m("CM","handing "+response);
+
+                ArrayList<String> playlists = new ArrayList<>();
+                try {
+                    JSONObject object = new JSONObject(response);
+
+                    int playlistCount = Integer.parseInt(object.getJSONObject("metadata").getString("count"));
+
+                    JSONArray playlistJsonArray = object.getJSONArray("results");
+
+                    for (int i = 0; i < playlistCount; i++) {
+
+                        requestTrendingType(playlistJsonArray.getJSONObject(i).getString("playlist"));
+
                     }
-                },
-                new Response.ErrorListener() {
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                //L.m("CM(test) ","Supported Playlist");
+
+            }
+
+            ///////////////////////////////////////////////////////////////////////////
+            // Results Section
+            ///////////////////////////////////////////////////////////////////////////
+
+
+            public void requestSearch(String term) {
+
+                String url = URLS.URL_SEARCH_RESULT + "q=" + URLEncoder.encode(term);
+                StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        handleResultResponse(response);
+                    }
+                }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError volleyError) {
+
+                        L.m("CM", "error " + volleyError);
 
                     }
                 });
 
-        playlistReq.setRetryPolicy(new DefaultRetryPolicy(
-                SERVER_TIMEOUT_LIMIT,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                request.setRetryPolicy(new DefaultRetryPolicy(SERVER_TIMEOUT_LIMIT,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
-        VolleyUtils.getInstance().addToRequestQueue(playlistReq, "playReq", context);
-    }
+                VolleyUtils.getInstance().addToRequestQueue(request, "resultReq", context);
+            }
 
-    private void handleSupportedPlaylists(String response) {
+            private void handleResultResponse(String response) {
 
-        //L.m("CM","handing "+response);
+                L.m("CM", "got result " + response);
+                ArrayList<ItemModel> songs = new ArrayList<>();
 
-        ArrayList<String> playlists = new ArrayList<>();
-        try {
-            JSONObject object = new JSONObject(response);
+                try {
+                    JSONObject rootObj = new JSONObject(response);
+                    int results_count = rootObj.getJSONObject("metadata").getInt("count");
 
-            int playlistCount = Integer.parseInt(object.getJSONObject("metadata").getString("count"));
+                    JSONArray results = rootObj.getJSONArray("results");
+                    for (int i = 0; i < results_count; i++) {
+                        String enc_v_id = results.getJSONObject(i).getString("get_url").substring(14);
+                        L.m("CM"," video id "+enc_v_id);
+                        songs.add(new ItemModel(results.getJSONObject(i).getString("title"),
+                                results.getJSONObject(i).getString("length"),
+                                results.getJSONObject(i).getString("uploader"),
+                                results.getJSONObject(i).getString("thumb"),
+                                enc_v_id,
+                                results.getJSONObject(i).getString("time"),
+                                results.getJSONObject(i).getString("views")
+                        ));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-            JSONArray playlistJsonArray = object.getJSONArray("results");
-
-            for (int i = 0; i < playlistCount; i++) {
-
-                requestTrendingType(playlistJsonArray.getJSONObject(i).getString("playlist"));
+                // database write test
+                dbHelper.addResultsList(new SectionModel("Results",songs));
 
             }
 
-
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
-
-        //L.m("CM(test) ","Supported Playlist");
-
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Results Section
-    ///////////////////////////////////////////////////////////////////////////
-
-
-    public void requestSearch(String term) {
-
-        String url = URLS.URL_SEARCH_RESULT + "q=" + URLEncoder.encode(term);
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                handleResultResponse(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-
-                L.m("CM", "error " + volleyError);
-
-            }
-        });
-
-        request.setRetryPolicy(new DefaultRetryPolicy(SERVER_TIMEOUT_LIMIT,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-
-        VolleyUtils.getInstance().addToRequestQueue(request, "resultReq", context);
-    }
-
-    private void handleResultResponse(String response) {
-
-        L.m("CM", "got result " + response);
-        ArrayList<ItemModel> songs = new ArrayList<>();
-
-        try {
-            JSONObject rootObj = new JSONObject(response);
-            int results_count = rootObj.getJSONObject("metadata").getInt("count");
-
-            JSONArray results = rootObj.getJSONArray("results");
-            for (int i = 0; i < results_count; i++) {
-                String enc_v_id = results.getJSONObject(i).getString("get_url").substring(14);
-                L.m("CM"," video id "+enc_v_id);
-                songs.add(new ItemModel(results.getJSONObject(i).getString("title"),
-                        results.getJSONObject(i).getString("length"),
-                        results.getJSONObject(i).getString("uploader"),
-                        results.getJSONObject(i).getString("thumb"),
-                        enc_v_id,
-                        results.getJSONObject(i).getString("time"),
-                        results.getJSONObject(i).getString("views")
-                ));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        // database write test
-        dbHelper.addResultsList(new SectionModel("Results",songs));
-
-    }
-
-}
