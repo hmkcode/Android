48c5e5bde2f15c291ab67d179c5852796ed713b6
rajesh65 <rajeshxreach@gmail.com>
2016-09-21 15:37:27 +0530

    added home activity

---
+++ TierScrollView/app/src/main/java/simple/tierscrollview/HomeFragment.java
@@ -0,0 +1,22 @@
+package simple.tierscrollview;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
