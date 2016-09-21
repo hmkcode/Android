10231214801d0df738a334fa4fa69a7931855e60
rajesh65 <rajeshxreach@gmail.com>
2016-09-21 16:20:13 +0530

    fix: right drawer

--- TierScrollView/app/src/main/java/simple/tierscrollview/FeedSyncService.java
+++ TierScrollView/app/src/main/java/simple/tierscrollview/FeedSyncService.java
@@ -4,13 +4,6 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p/>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
 public class FeedSyncService extends IntentService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
@@ -21,12 +14,6 @@ public class FeedSyncService extends IntentService {
     private static final String EXTRA_PARAM1 = "simple.tierscrollview.extra.PARAM1";
     private static final String EXTRA_PARAM2 = "simple.tierscrollview.extra.PARAM2";
 
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
     // TODO: Customize helper method
     public static void startActionFoo(Context context, String param1, String param2) {
         Intent intent = new Intent(context, FeedSyncService.class);
@@ -35,13 +22,6 @@ public class FeedSyncService extends IntentService {
         intent.putExtra(EXTRA_PARAM2, param2);
         context.startService(intent);
     }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
     // TODO: Customize helper method
     public static void startActionBaz(Context context, String param1, String param2) {
         Intent intent = new Intent(context, FeedSyncService.class);
@@ -70,20 +50,10 @@ public class FeedSyncService extends IntentService {
             }
         }
     }
-
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
     private void handleActionFoo(String param1, String param2) {
         // TODO: Handle action Foo
         throw new UnsupportedOperationException("Not yet implemented");
     }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
     private void handleActionBaz(String param1, String param2) {
         // TODO: Handle action Baz
         throw new UnsupportedOperationException("Not yet implemented");
--- TierScrollView/app/src/main/java/simple/tierscrollview/util/SystemUiHiderHoneycomb.java
+++ TierScrollView/app/src/main/java/simple/tierscrollview/util/SystemUiHiderHoneycomb.java
@@ -6,42 +6,17 @@ import android.os.Build;
 import android.view.View;
 import android.view.WindowManager;
 
-/**
- * An API 11+ implementation of {@link SystemUiHider}. Uses APIs available in
- * Honeycomb and later (specifically {@link View#setSystemUiVisibility(int)}) to
- * show and hide the system UI.
- */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class SystemUiHiderHoneycomb extends SystemUiHiderBase {
-    /**
-     * Flags for {@link View#setSystemUiVisibility(int)} to use when showing the
-     * system UI.
-     */
+
     private int mShowFlags;
 
-    /**
-     * Flags for {@link View#setSystemUiVisibility(int)} to use when hiding the
-     * system UI.
-     */
     private int mHideFlags;
 
-    /**
-     * Flags to test against the first parameter in
-     * {@link android.view.View.OnSystemUiVisibilityChangeListener#onSystemUiVisibilityChange(int)}
-     * to determine the system UI visibility state.
-     */
     private int mTestFlags;
 
-    /**
-     * Whether or not the system UI is currently visible. This is cached from
-     * {@link android.view.View.OnSystemUiVisibilityChangeListener}.
-     */
     private boolean mVisible = true;
 
-    /**
-     * Constructor not intended to be called by clients. Use
-     * {@link SystemUiHider#getInstance} to obtain an instance.
-     */
     protected SystemUiHiderHoneycomb(Activity activity, View anchorView, int flags) {
         super(activity, anchorView, flags);
 
