afcef7417a18effc27f5a1cc73c61d1f1dfc812f
rajesh65 <rajeshxreach@gmail.com>
2016-09-21 15:36:46 +0530

    added some UI components

--- TierScrollView/app/src/main/java/simple/tierscrollview/PlusOneFragment.java
+++ TierScrollView/app/src/main/java/simple/tierscrollview/PlusOneFragment.java
@@ -10,14 +10,6 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.plus.PlusOneButton;
 
-/**
- * A fragment with a Google +1 button.
- * Activities that contain this fragment must implement the
- * {@link PlusOneFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PlusOneFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PlusOneFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -96,33 +88,6 @@ public class PlusOneFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
