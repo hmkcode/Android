3b5ab94170b5545009785675dcae07eeefd89de8
rajesh65 <rajeshxreach@gmail.com>
2016-09-23 20:02:37 +0530

    invalidated menu items

--- PlusMortProject/app/src/main/java/simple/plusmortproject/IntentServiceMort.java
+++ PlusMortProject/app/src/main/java/simple/plusmortproject/IntentServiceMort.java
@@ -3,6 +3,11 @@ package simple.plusmortproject;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.os.IBinder;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 
 
 public class IntentServiceMort extends IntentService {
@@ -39,5 +44,74 @@ public class IntentServiceMort extends IntentService {
     private void handleActionBaz(String param1, String param2) {
         // TODO: Handle action Baz
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public IntentServiceMort(String name) {
+        super(name);
+    }
+
+    @Override
+    public void setIntentRedelivery(boolean enabled) {
+        super.setIntentRedelivery(enabled);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+    }
+
+    @Override
+    protected void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
+        super.dump(fd, writer, args);
     }
 }
