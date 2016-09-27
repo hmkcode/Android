04843e5d0b9aa7e4de1b44acb7692650c5a2fcb2
rajesh65 <rajeshxreach@gmail.com>
2016-09-28 01:53:52 +0530

    added view

--- CardApproach/app/src/main/java/simple/cardapproach/MainActivity.java
+++ CardApproach/app/src/main/java/simple/cardapproach/MainActivity.java
@@ -14,12 +14,3 @@ protected void onCreate(Bundle savedInstanceState) {
         R.id.navigation_drawer,
         (DrawerLayout) findViewById(R.id.drawer_layout));
         }
-
-@Override
-public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-        .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-        .commit();
-        }
