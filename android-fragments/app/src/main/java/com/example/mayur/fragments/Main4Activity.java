54470fcf31be737420dc60cd445f32210faa7190
Mayur Shastri <mayurshastri97@gmail.com>
2017-10-20 13:18:43 +0530

    fragments example added

---
+++ android-fragments/app/src/main/java/com/example/mayur/fragments/Main4Activity.java
@@ -0,0 +1,18 @@
+package com.example.mayur.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Main4Activity extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main4,container,false);
+        return view;
+    }
+}
