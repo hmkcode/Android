1339ed32fb8705092bb5459abee6af06e1f16bca
Hani M. K <hmkcode@gmail.com>
2017-12-22 23:53:27 +0300

    add tablayout
    add tablayout

--- user-interface/android-swipe-views-tabs/app/src/main/java/com/hmkcode/android/MainActivity.java
+++ user-interface/android-swipe-views-tabs/app/src/main/java/com/hmkcode/android/MainActivity.java
@@ -3,6 +3,7 @@ package com.hmkcode.android;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 
 import com.hmkcode.android.adapters.MyFragmentPagerAdapter;
 
@@ -10,17 +11,32 @@ public class MainActivity extends AppCompatActivity {
 
     MyFragmentPagerAdapter myFragmentPagerAdapter;
     ViewPager mViewPager;
-
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        setPagerAdapter();
+        setTabLayout();
+    }
+
+    private void setPagerAdapter(){
 
         myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(myFragmentPagerAdapter);
 
+    }
+
+    private void setTabLayout() {
+
+        tabLayout.setupWithViewPager(mViewPager);
+
+        tabLayout.getTabAt(0).setText("First");
+        tabLayout.getTabAt(1).setText("Second");
     }
 }
