87f6be7484a254f7d91b5c3554dc9f711461495c
Hani M. K <hmkcode@gmail.com>
2019-10-08 20:37:49 +0300

    android connectivity

---
+++ android-connectivity/app/src/main/java/com/hmkcode/connectivity/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.hmkcode.connectivity
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var tvConnectivity: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvConnectivity = findViewById<TextView>(R.id.tvConnectivity)
+
+        // get ConnectivityManager
+        val cm:ConnectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val builder: NetworkRequest.Builder = NetworkRequest.Builder()
+            cm.registerNetworkCallback(
+
+                builder.build(),
+                object : ConnectivityManager.NetworkCallback() {
+
+                    override fun onAvailable(network: Network) {
+                        lifecycleScope.launch {
+                            Log.i("MainActivity", "onAvailable!"+network.toString())
+
+                            // check if NetworkCapabilities has TRANSPORT_WIFI
+                            val isWifi:Boolean = cm.getNetworkCapabilities(network).hasTransport(
+                                NetworkCapabilities.TRANSPORT_WIFI)
+
+                            doSomething(true, isWifi)
+                        }
+                    }
+
+                    override fun onLost(network: Network) {
+                        lifecycleScope.launch {
+                            Log.i("MainActivity", "onLost!"+network.toString())
+                            doSomething(false)
+                        }
+                    }
+                }
+            )
+        }
+
+    }
+
+    private suspend fun doSomething(isConnected:Boolean, isWifi:Boolean= false){
+        withContext(Dispatchers.Main){
+            if(isConnected) {
+                tvConnectivity.text = "Connected "+(if(isWifi)"WIFI" else "MOBILE")
+                tvConnectivity.setBackgroundColor(-0x8333da)
+            }else {
+                tvConnectivity.text = "Not Connected"
+                tvConnectivity.setBackgroundColor(-0x10000)
+            }
+        }
+    }
+}
