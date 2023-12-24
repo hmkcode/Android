abf4d0d47ae2bc41079ef542821b0fabf9b6a1d8
Raj-Pavara <151735565+Raj-Pavara@users.noreply.github.com>
2023-12-24 22:22:48 +0530

    Update MainActivity.java

--- android-L-preview/src/com/hmkcode/android/L/MainActivity.java
+++ android-L-preview/src/com/hmkcode/android/L/MainActivity.java
@@ -9,6 +9,4 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-}
+		setContentView(R.layout.activity
